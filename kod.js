1a8338040e0c69dc158b9ca74aceb2064758bcf9
Mostafa <mostafahassan93@hotmail.com>
2024-09-08 15:17:44 +0200

    Added functions for fetching society id and facility ids

--- kod.js
+++ kod.js
@@ -1,17 +1,21 @@
 const boappa_api_url = 'https://wapi.boappa.se';
 
 function main() {
-  const credentials = loginToBoAppaAndGetCredentials()
-  console.log("token is: " + credentials.token)
-  console.log("user id is: " + credentials.user_id)
+  const credentials = LoginToBoAppaAndGetCredentials()
+  //console.log("token is: " + credentials.token)
+  //console.log("user id is: " + credentials.user_id)
 
-  //createCalendarEvents()
+  const society_id = GetSocietyId(credentials.user_id, credentials.token)
+  console.log(`Society id is: ${society_id}`)
+
+  const facilities = GetFacilities(credentials.user_id, credentials.token, society_id)
+  console.log(facilities)
 }
   
-function loginToBoAppaAndGetCredentials(){
+function LoginToBoAppaAndGetCredentials(){
   var boappa_username = PropertiesService.getScriptProperties().getProperty('boappa_username');
   var boappa_password = PropertiesService.getScriptProperties().getProperty('boappa_password');
-
+  const url = `${boappa_api_url}/users/login`
   
   var payload = JSON.stringify({
     email: boappa_username,
@@ -29,7 +33,6 @@ function loginToBoAppaAndGetCredentials(){
     headers: headers
   };
 
-  const url = boappa_api_url + "/users/login"
   var response = UrlFetchApp.fetch(url, options);
   var jsonData = JSON.parse(response.getContentText());
 
@@ -40,4 +43,62 @@ function loginToBoAppaAndGetCredentials(){
     token: token,
     user_id: user_id
   };
+}
+
+function GetSocietyId(user_id, token){
+  const url = `${boappa_api_url}/units/get-user-units/${user_id}`
+
+  var headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${token}`
+  };
+
+  var options = {
+    method: 'get',
+    contentType: 'application/json',
+    headers: headers
+  };
+
+  var response = UrlFetchApp.fetch(url, options);
+  var jsonData = JSON.parse(response.getContentText());
+  if(jsonData.data.length > 0)
+  {
+    return jsonData.data[0].societyId
+  }
+  return null
+}
+
+function GetFacilities(user_id, token, society_id){
+  const url = `${boappa_api_url}/facilities/get-per-societies/?populate=media`
+
+  var headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${token}`
+  };
+
+  var payload = JSON.stringify({
+    societyIds: [society_id]
+  });
+
+  var options = {
+    method: 'get',
+    contentType: 'application/json',
+    payload: payload,
+    headers: headers
+  };
+
+  var response = UrlFetchApp.fetch(url, options);
+  var jsonData = JSON.parse(response.getContentText());
+  if(jsonData.data.length > 0)
+  {
+    var facilitiy_ids = [];
+    for(var x in jsonData.data)
+    {
+      var facility_id = jsonData.data[x]._id;
+      facilitiy_ids.push(facility_id)
+
+    }
+    return facilitiy_ids
+  }
+  return null
 }
